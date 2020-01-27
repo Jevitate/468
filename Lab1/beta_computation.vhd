416cff66c02fe9b3dcd37bc61a5a4096d07ca3fc
Josh Freund <mc_gruber123@yahoo.com>
2020-01-27 00:15:47 -0700

    update to vhdl files

--- Lab1/beta_computation.vhd
+++ Lab1/beta_computation.vhd
@@ -9,11 +9,11 @@ entity beta_computation is
 	generic(
 		W_bits			: positive := 32; -- size of word
 		F_bits			: positive := 16; -- number of fractional bits
-		N_iterations	: positive := 3); -- number of newton's iterations
+		Z_bits			: positive);		-- leading zeros
 		
 	port(
-		x	: in std_logic_vector(W_bits-1 downto 0);
-		y	: out std_logic_vector(W_bits-1 downto 0));
+
+		beta	: out integer);				
 		
 end entity beta_computation;
 
@@ -21,7 +21,14 @@ end entity beta_computation;
 
 architecture beta_comp_arch of beta_computation is
 
+	----Beta = W - F - Z - 1
+	
+	
+
 	begin
+	
+	
+	beta <= W - F - Z - 1;
 	
 	
 end architecture;
